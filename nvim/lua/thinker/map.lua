884a375a433e0bb43feb280686858fd26c9575b4
CobaltFrog <vladislavtemnov2019@gmail.com>
2026-03-18 19:21:21 +0300

    add nvim config and theme "thinker"

---
+++ nvim/lua/thinker/map.lua
@@ -0,0 +1,45 @@
+local lg_opt = { noremap = true, silent = true } -- l(ocal)g(lobal) option
+
+local function map(mode, lhs, rhs, opts_arg, desc_arg)
+    local opts = {}
+
+    if type(opts_arg) == "table" then
+        for k, v in pairs(opts_arg) do
+            opts[k] = v
+        end
+    elseif type(opts_arg) == "string" then
+        opts.desc = opts_arg
+    end
+
+    if desc_arg ~= nil then
+        opts.desc = desc_arg
+    end
+
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- MAPPINGS --
+map('n', 't', ':NvimTreeToggle<CR>', lg_opt, "Toggle files tree")
+
+-- buffer 
+map('n', '<C-s>', ':write<CR>', lg_opt, "Write buffer to current file")
+map('n', '<Esc>', ':noh<CR>', lg_opt, "Clear search highlight")
+
+-- split movement
+map("n", "<C-h>", "<C-w>h", "Move left")
+map("n", "<C-j>", "<C-w>j", "Move down")
+map("n", "<C-k>", "<C-w>k", "Move up")
+map("n", "<C-l>", "<C-w>l", "Move right")
+
+-- clipboard
+map('n', '<leader>r', '"+', lg_opt, "Start command for '+' register")
+map('v', '<leader>c', '"+y', lg_opt, "Copy to clipboard")
+map('n', '<leader>c', '"+yy', lg_opt, "Copy line to clipboard")
+map('n', '<leader>v', '"+p', lg_opt, "Paste from clipboard after cursor")
+map('n', '<leader>V', '"+P', lg_opt, "Paste from clipboard before cursor")
+
+-- visual
+map('n', '<leader>T', ':TransparentToggle<CR>', lg_opt)
+
+-- plugins
+map('n', '<leader>"', 'g@iw"')
---
+++ nvim/lua/thinker/opts.lua
@@ -0,0 +1,22 @@
+-- TABULATION --
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- SEARCH --
+vim.opt.smartcase = true
+
+-- BUFFERS --
+vim.opt.undofile = true
+vim.opt.swapfile = false
+
+-- VISUAL --
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+
